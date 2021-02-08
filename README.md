66b16fa685e6bcdad2fd84bc7f6bed9834d6b245
ribbon-comm <78724286+ribbon-comm@users.noreply.github.com>
2021-02-08 14:00:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WebApp
+ Ribbon Technothon
---
+++ WebApplication/pageone/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+# Create your views here.
+def login(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username,password)
+        return redirect('/')
+    else:
+        return render(request,"login.html")
